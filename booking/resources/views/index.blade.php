0357496d3a3495ae966ec5b287007cea17e2c520
Brian Rios <briosm0601@gmail.com>
2023-10-03 13:03:19 -0500

    estructura

---
+++ booking/resources/views/index.blade.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="bg-black">
+        <nav>
+            <ul>
+                <a href="#">Tours</a>
+                <a href="#">Experiences</a>
+                <a href="#">Activities</a>
+                <a href="#">Restaurants</a>
+                <a href="#">Transportation</a>
+            </ul>
+        </nav>
+
+        <div>
+            <input type="text" placeholder="Where's your next adventure?">
+        </div>
+
+        <div>
+            <input type="text" placeholder="Where are you going?">
+            <input type="text" placeholder="Check-in Date">
+            <input type="text" placeholder="Adults">
+            <input type="text" placeholder="Kids">
+            <button>SEARCH</button>
+        </div>
+
+        <div>
+            <h1 class="text-3xl font-bold underline">We Set-Up Your Trip</h1>
+            <p>
+                We take care of putting together and booking all the
+                experiences you need on your trip. On our platform
+                you can organize your itinerary, and have everything
+                you need on your adventure.
+            </p>
+            <a href="#">Download it NOW!</a>
+        </div>
+
+        <div>
+            <ul>
+                <a href="#">Tours</a>
+                <a href="#">Experiences</a>
+                <a href="#">Activities</a>
+                <a href="#">Restaurants</a>
+                <a href="#">Transportation</a>
+            </ul>
+
+            <img src="" alt="">
+            <img src="" alt="">
+        </div>
+
+        <div>
+            <h1>Travel With Confidence</h1>
+            <p>
+                We want to take care of every detail of your adventures
+                with us. For that reason, we have created an itinerary
+                for you, with all the specifics and information, for
+                you to be able to have the best time.
+            </p>
+
+            <a href="#">Download it NOW!</a>
+        </div>
+
+        <div> 
+            <h2>Popular Destinations</h2>
+            <div>
+                <img src="" alt="">
+                <h3>Holbox</h3>
+            </div>
+
+            <div>
+                <img src="" alt="">
+                <h3>Tulum</h3>
+            </div>
+
+            <div>
+                <img src="" alt="">
+                <h3>Playa del Carmen</h3>
+            </div>
+
+            <div>
+                <img src="" alt="">
+                <h3>Bacalar</h3>
+            </div>
+
+            <div>
+                <img src="" alt="">
+                <h3>Mahahual</h3>
+            </div>
+
+            <div>
+                <img src="" alt="">
+                <h3>Isla Mujeres</h3>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
