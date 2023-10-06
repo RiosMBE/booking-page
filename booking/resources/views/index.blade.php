fe3e168532c9104514f926c32b53b73572cfe7a8
Brian Rios <briosm0601@gmail.com>
2023-10-06 14:48:14 -0500

    Diseño actualizado

--- booking/resources/views/index.blade.php
+++ booking/resources/views/index.blade.php
@@ -7,43 +7,47 @@
   @vite('resources/css/app.css')
 </head>
 <body>
-    <div class="container max-w-full flex-auto">
-        <div class="bg-blue-950">
-            <nav class="mx-xl py-10  text-center text-white">
+    <div class="md:container md:mx-auto">
+
+        <nav class="bg-blue-950 py-[3rem]">
+            <div class="text-center text-white">
                 <ul>
                     <li>
-                        <a href="#">Tours</a>
-                        <a href="#">Experiences</a>
-                        <a href="#">Activities</a>
-                        <a href="#">Restaurants</a>
-                        <a href="#">Transportation</a>
+                        <a class="py-[14px] px-[20px] hover:underline hover:text-gray-500" href="#">Tours</a>
+                        <a class="py-[14px] px-[20px] hover:underline hover:text-gray-500" href="#">Experiences</a>
+                        <a class="py-[14px] px-[20px] hover:underline hover:text-gray-500" href="#">Activities</a>
+                        <a class="py-[14px] px-[20px] hover:underline hover:text-gray-500" href="#">Restaurants</a>
+                        <a class="py-[14px] px-[20px] hover:underline hover:text-gray-500" href="#">Transportation</a>
                     </li>
                 </ul>
-            </nav>
-        </div>
+            </div>
+        </nav>
 
-        <div class="bg-cover bg-center h-96" style="background-image: url('img/playa.jpg')">
+        <div class="bg-cover bg-center h-[60vh]" style="background-image: url('img/playa.jpg')">
             <div class="flex justify-center">
-                <input class="w-2/4 p-4 rounded-lg border text-center" type="text" placeholder="Where's your next adventure?">
+                <input class="mt-[40vh] w-2/4 p-4 rounded-xl border text-center" type="text" placeholder="Where's your next adventure?">
             </div>
         </div>
 
-        <section class="bg-blue-950 py-6 px-5 rounded-lg">
-            <div>
-                <input type="text" placeholder="Where are you going!">
-                <input type="text" placeholder="Check-In Date">
-                <input type="text" placeholder="Adults">
-                <input type="text" placeholder="Kids">    
+        <section class="flex justify-center pb-[2.7rem] -mt-[2.5rem]">
+            <div class="bg-blue-950 rounded-lg grid grid-rows-2 p-6">
+                <div class="text-[1.1rem]">
+                    <input type="text" placeholder="Where are you going!">
+                    <input type="text" placeholder="Check-In Date">
+                    <input type="text" placeholder="Adults">
+                    <input type="text" placeholder="Kids">    
+                </div>
+
+                <div class="flex justify-center">
+                    <button class="bg-blue-300 hover:bg-blue-500 text-white font-bold py-2 px-18 rounded-lg w-3/5">SEARCH</button>
+                </div>
             </div>
-
-            <button class="bg-blue-300 hover:bg-blue-500 text-white font-bold py-2 px-18 rounded-lg">SEARCH</button>
-
         </section>
 
-        <section class="bg-blue-50 flex-auto">
-            <div>
-                <h1 class="text-2xl">We Set-Up Your Trip</h1>
-                <p class="text-gray-600 text-justify">
+        <section class="bg-blue-50 flex flex-auto justify-center">
+            <div class="p-10">
+                <h1 class="text-2xl pb-6">We Set-Up Your Trip</h1>
+                <p class="text-gray-600 text-justify pb-6">
                     We take care of putting together and booking all
                     the experiences you need on your trip. On our
                     platform you can organize your itinerary, and
@@ -54,29 +58,42 @@
         </section>
 
         <section class="flex justify-center">
-            <ul>
-                <li>
-                    <a href="#">Tours</a>
-                    <a href="#">Experiences</a>
-                    <a href="#">Activities</a>
-                    <a href="#">Restaurants</a>
-                    <a href="#">Transportation</a>
-                </li>
-            </ul>
+            <div class="">
+                <nav class="text-center text-blue-700 font-light hover:text-blue-900 text-lg pt-[3.2rem] text-[1.6rem]">
+                    <ul>
+                        <li>
+                            <a class="py-[0.70rem] px-[2rem] hover:inderline" href="#">Tours</a>
+                            <a class="py-[0.70rem] px-[2rem] hover:underline" href="#">Experiences</a>
+                            <a class="py-[0.70rem] px-[2rem] hover:underline" href="#">Activities</a>
+                            <a class="py-[0.70rem] px-[2rem] hover:underline" href="#">Restaurants</a>
+                            <a class="py-[0.70rem] px-[2rem] hover:underline" href="#">Transportation</a>
+                        </li>
+                    </ul>
+                </nav>
 
-            <img src="" alt="">
-            <img src="" alt="">
+                <div class="flex flex-auto pt-[4vh] pb-[6vh]">
+                    <div class="w-[45rem] pr-[4rem]">
+                        <img class="rounded-lg" src="{{asset('img/section 2 (1).png')}}" alt="1">
+                    </div>
+
+                    <div class="w-[45rem] pl-[4rem]">
+                        <img class="rounded-lg" src="{{asset('img/section 2 (2).jpg')}}" alt="2">
+                    </div>
+                </div>
+            </div>
         </section>
 
-        <section class="bg-blue-50">
-            <h1 class="text-2xl">Travel With Confidence</h1>
-            <p class="text-gray-600 text-justify">
-                We want to take care of every detail of your adventures
-                with us. For that reason, we have created an itinerary
-                for you, with all the specifics and information, for you
-                to be able to have the best time.
-            </p>
-            <a class="text-xl text-blue-900" href="#">Download it NOW!</a>
+        <section class="bg-blue-50 flex flex-auto justify-center">
+            <div class="p-10">
+                <h1 class="text-2xl pb-6">Travel With Confidence</h1>
+                <p class="text-gray-600 text-justify pb-6">
+                    We want to take care of every detail of your adventures
+                    with us. For that reason, we have created an itinerary
+                    for you, with all the specifics and information, for you
+                    to be able to have the best time.
+                </p>
+                <a class="text-xl text-blue-900" href="#">Download it NOW!</a>
+            </div>
         </section>
     </div>
 </body>
